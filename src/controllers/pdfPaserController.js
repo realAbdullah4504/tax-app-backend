948ce2eab3c399d4f2d2b1f6731b48474c3d4026
Asad Sheikh <33250357+Sheikhasadde@users.noreply.github.com>
2023-10-01 18:39:37 +0500

    file upload and pdf base64 string is in done for pdf parser

--- src/controllers/pdfPaserController.js
+++ src/controllers/pdfPaserController.js
@@ -1,25 +1,60 @@
 const pdfParserService = require("../services/pdfParserService");
 const sendAppResponse = require("../utils/helper/appResponse");
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs");
+const uploadDi = "src/uploads";
+
 exports.pdfParser = async (req, res, next) => {
   try {
     let data = {};
-    const { fileString, docType } = req.body;
-    const endPoint = await pdfParserService.pdfEndPoints(docType);
-    const source = await pdfParserService.inputSource(fileString, docType);
-    const pdfData = await pdfParserService.parsePDF(source, endPoint);
-    const fields = pdfData?.document?.inference?.prediction?.fields;
-    fields.forEach((element, key) => {
-      data[key] = element.values.join(" ");
+    let fileString;
+    const { docType } = req.params;
+
+    const form = new formidable.IncomingForm();
+    form.uploadDir = uploadDi;
+    form.multiples = false;
+    form.keepExtensions = true;
+    form.maxFieldsSize = 10 * 1024 * 1024;
+
+    form.parse(req, async (_err, fields, files) => {
+      if (_err)
+        sendAppResponse({
+          res,
+          _err,
+          statusCode: 400,
+          status: "failed",
+          message: "file upload failed",
+        });
     });
 
-    console.log(data);
-    sendAppResponse({
-      res,
-      data,
-      statusCode: 200,
-      status: "success",
-      message: "PDF parser executed.",
+    form.on("file", async (name, file) => {
+      fileString = await fs.readFileSync(file.filepath, { encoding: "base64" });
+      const endPoint = await pdfParserService.pdfEndPoints(docType);
+      const source = await pdfParserService.inputSource(fileString, docType);
+      const pdfData = await pdfParserService.parsePDF(source, endPoint);
+      const pdfFields = pdfData?.document?.inference?.prediction?.fields;
+      pdfFields.forEach((element, key) => {
+        data[key] = element.values.join(" ");
+      });
+      sendAppResponse({
+        res,
+        data,
+        statusCode: 200,
+        status: "success",
+        message: "PDF parser executed.",
+      });
     });
+
+    // const { fileString, docType } = req.body;
+    // const endPoint = await pdfParserService.pdfEndPoints("employment-summery");
+    // const source = await pdfParserService.inputSource(fileString, docType);
+    // const pdfData = await pdfParserService.parsePDF(source, endPoint);
+    // const fields = pdfData?.document?.inference?.prediction?.fields;
+    // fields.forEach((element, key) => {
+    //   data[key] = element.values.join(" ");
+    // });
+    // console.log(fileString);
   } catch (error) {
     next(error);
   }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const UserController = require('../controllers/userController');
-const pdfParserController = require('../controllers/pdfPaserController');
-const AuthController = require('../controllers/authController');
-const { authenticate } = require('../middlewares/auth');
+const express = require("express");
+const UserController = require("../controllers/userController");
+const pdfParserController = require("../controllers/pdfPaserController");
+const AuthController = require("../controllers/authController");
+const { authenticate } = require("../middlewares/auth");
 const { signUp, verifyCode, login } = AuthController;
 const { getUserDetail, updateUserDetail } = UserController;
 const { pdfParser } = pdfParserController;
@@ -10,17 +10,17 @@ const {
   createUserValidator,
   verifyCodeValidator,
   loginUserValidator,
-} = require('../middlewares/validators');
+} = require("../middlewares/validators");
 const router = express.Router();
 
-router.post('/register', createUserValidator, signUp);
-router.post('/verify-code', [authenticate, verifyCodeValidator], verifyCode);
-router.post('/login', loginUserValidator, login);
+router.post("/register", createUserValidator, signUp);
+router.post("/verify-code", [authenticate, verifyCodeValidator], verifyCode);
+router.post("/login", loginUserValidator, login);
 
 // User
-router.get('/detail', authenticate, getUserDetail);
-router.post('/pdf', authenticate, pdfParser);
-router.post('/update', authenticate, updateUserDetail);
+router.get("/detail", authenticate, getUserDetail);
+router.post("/pdf/:docType", authenticate, pdfParser);
+router.post("/update", authenticate, updateUserDetail);
 // Protected route using the authenticate middleware
 
 module.exports = router;
