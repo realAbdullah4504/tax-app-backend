a5166b1e72b6f1dd274a4cb8a3d36e55e3e17748
aqdas <aqdasali136@gmail.com>
2023-11-14 22:39:36 +0500

    conflicts resolved

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -10,7 +10,6 @@ const createUserValidator = [
     .withMessage('Phone Number is required')
     // .matches(/^(?:\+92|0092|0)?(?:3[0-5][0-9]{8})$/)//for pakistani phone number
     .matches(/^(?:\+353|0353|0)?(?:[0-9]{9})$/) //for irish phone number
-    .withMessage('Invalid Pakistani phone number')
     .withMessage('Invalid Pakistani phone number'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
   body('tob').notEmpty().isBoolean().withMessage('Term & conditions are required to check'),
