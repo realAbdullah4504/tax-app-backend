628f9390fd022d834f06309d4265f78cc4c05c91
Asad Sheikh <33250357+Sheikhasadde@users.noreply.github.com>
2023-12-07 22:01:36 +0500

    type "single married2incomes etc" is included in payload and model.

--- src/controllers/taxRatesController.js
+++ src/controllers/taxRatesController.js
@@ -29,14 +29,13 @@ const calculate = async (year, userId) => {
   let uscPaid = 0;
 
   summaryDetails?.forEach((item) => {
-
     if (item?.summary_type === 'Self') {
       grossIncomeUscSelf = item.gross_pay;
     }
     if (item?.summary_type === 'Spouse') {
       grossIncomeUscSpouse = item.gross_pay;
     }
-    grossIncomeUsc = grossIncomeUscSelf + grossIncomeUscSpouse
+    grossIncomeUsc = grossIncomeUscSelf + grossIncomeUscSpouse;
     grossTaxableIncome += item.pay_for_income_tax;
     taxPaid += item.income_tax_paid;
     uscPaid += item.usc_paid;
@@ -245,7 +244,7 @@ const calculate = async (year, userId) => {
   const widow =
     totalYearsPassedSpouse > 0
       ? (widowCreditYearly || 0) -
-      ((totalYearsPassedSpouse > 5 ? 5 : totalYearsPassedSpouse) - 1) * 450
+        ((totalYearsPassedSpouse > 5 ? 5 : totalYearsPassedSpouse) - 1) * 450
       : 0;
   let widowTrail = maritalStatus === 'widowed' ? widow : 0; //formula should be understand from questions app
   // Carer Missed for married (Question app M37)
@@ -434,11 +433,14 @@ const calculate = async (year, userId) => {
     const usc1 = (Math.min(grossIncomeUsc, uscBands[0]) * uscRatesPercentage[0]) / 100;
     console.log('usc1', usc1);
 
-    const usc2 = (Math.min(grossIncomeUsc - uscBands[1], uscBands[1]) * uscRatesPercentage[1]) / 100;
+    const usc2 =
+      (Math.min(grossIncomeUsc - uscBands[1], uscBands[1]) * uscRatesPercentage[1]) / 100;
     console.log('usc2', usc2);
 
     const usc3 =
-      (Math.min(grossIncomeUsc - (uscBands[0] + uscBands[1]), uscBands[2]) * uscRatesPercentage[2]) / 100;
+      (Math.min(grossIncomeUsc - (uscBands[0] + uscBands[1]), uscBands[2]) *
+        uscRatesPercentage[2]) /
+      100;
     console.log('usc3', usc3);
     const usc4 =
       ((grossIncomeUsc - (uscBands[0] + uscBands[1] + uscBands[2])) *
@@ -454,15 +456,20 @@ const calculate = async (year, userId) => {
     );
     console.log('usc4', usc4);
 
-
     //add total
     const totalUsc = usc1 + usc2 + usc3 + usc4;
     return { totalUsc, usc1, usc2, usc3, usc4 };
     // console.log('totalUsc', totalUsc);
     // return totalUsc;
-  }
+  };
   const { totalUsc: totalUscSelf, usc1, usc2, usc3, usc4 } = uscCalculation(grossIncomeUscSelf);
-  const { totalUsc: totalUscSpouse, usc1: spouseUsc1, usc2: spouseUsc2, usc3: spouseUsc3, usc4: spouseUsc4 } = type === 'married2Incomes' ? uscCalculation(grossIncomeUscSpouse) : {};
+  const {
+    totalUsc: totalUscSpouse,
+    usc1: spouseUsc1,
+    usc2: spouseUsc2,
+    usc3: spouseUsc3,
+    usc4: spouseUsc4,
+  } = type === 'married2Incomes' ? uscCalculation(grossIncomeUscSpouse) : {};
 
   const totalUsc = totalUscSelf + (totalUscSpouse || 0);
 
@@ -480,6 +487,7 @@ const calculate = async (year, userId) => {
   console.log('finalFigureForCustomer', finalFigureForCustomer);
 
   const payload = {
+    type,
     year,
     grossIncomeUsc,
     grossTaxableIncome,
@@ -608,9 +616,9 @@ exports.getCalculations = async (req, res, next) => {
 
 /**
  * controller function to update tax default values
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
  */
 exports.updateDefaultTaxValues = async (req, res, next) => {
   try {
@@ -619,7 +627,7 @@ exports.updateDefaultTaxValues = async (req, res, next) => {
       { year: yearToUpdate },
       req.body,
       {
-        upsert: true
+        upsert: true,
       }
     );
     // if (!updatedTaxRates) throw new AppError("Year not found", 404);
@@ -634,5 +642,4 @@ exports.updateDefaultTaxValues = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-  
-}
+};
