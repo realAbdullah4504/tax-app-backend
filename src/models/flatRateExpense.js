b149511c70d1d2e7785ac1f8900818d81c5826ea
UsmanGhani6217 <bossbig6217@gmail.com>
2023-12-13 23:40:21 +0500

    worked on add and get categories and sub categories

--- src/models/flatRateExpense.js
+++ src/models/flatRateExpense.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const flatRateExpenseSchema = new mongoose.Schema({
   year:Number,
+  industryNotListed:Number,
   actorFreelance:{
     freeLanceActorChargeablePaye:Number
   },
