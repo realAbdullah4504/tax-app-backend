57911f5cf1f1d4bc9e504c09bf0da7e9b0f7bc2d
aqdas <aqdasali136@gmail.com>
2023-12-14 00:41:00 +0500

    regix update for mobile number

--- src/middlewares/validators.js
+++ src/middlewares/validators.js
@@ -8,7 +8,7 @@ const createUserValidator = [
     .notEmpty()
     .withMessage('Phone Number is required')
     // .matches(/^(?:\+92|0092|0)?(?:3[0-5][0-9]{8})$/)//for pakistani phone number
-    .matches(/^(?:\+353|0353|0)?(?:[0-9]{9})$/) //for irish phone number
+    .matches(/^(?:\+353|0353|0)?(?:[0-9]{9,10})$/) //for irish phone number
     .withMessage('Invalid Pakistani phone number'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
   body('tob').notEmpty().isBoolean().withMessage('Term & conditions are required to check'),
